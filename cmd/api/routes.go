5127b32cc0f87eb85815b5de90743513e57ca08a
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2025-07-08 21:44:59 +0200

    ci: added htmx, templ libs

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/bujdoluk/go-htmx-templ/views"
 	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) mainHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+	views.Index().Render(r.Context(), w)
+}
+
+func (app *application) helloHandler(w http.ResponseWriter, r *http.Request) {
+	views.Hello().Render(r.Context(), w)
 }
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/", app.mainHandler)
+	router.HandlerFunc(http.MethodGet, "/hello", app.helloHandler)
+
+	router.ServeFiles("/static/*filepath", http.Dir("static"))
 
 	return router
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/bujdoluk/go-htmx-templ
 go 1.24.4
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/a-h/templ v0.3.906 // indirect
